506f27b6dbfccf444cdd00b48a4897274f6aaf16
StefanAnaniev <ananiev.stefan@gmail.com>
2021-09-05 19:03:29 +0200

    CRUD Operations

--- TechTreeMVCWebApplication/Areas/Admin/Controllers/CategoryItemController.cs
+++ TechTreeMVCWebApplication/Areas/Admin/Controllers/CategoryItemController.cs
@@ -98,11 +98,16 @@ namespace TechTreeMVCWebApplication.Areas.Admin.Controllers
                 return NotFound();
             }
 
+            List<MediaType> mediaTypes = await _context.MediaType.ToListAsync();
+
             var categoryItem = await _context.CategoryItem.FindAsync(id);
             if (categoryItem == null)
             {
                 return NotFound();
             }
+
+            categoryItem.MediaTypes = mediaTypes.ConvertToSelectList(categoryItem.MediaTypeId);
+
             return View(categoryItem);
         }
 
@@ -136,7 +141,7 @@ namespace TechTreeMVCWebApplication.Areas.Admin.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index), new { categoryId = categoryItem.CategoryId });
             }
             return View(categoryItem);
         }
@@ -167,7 +172,7 @@ namespace TechTreeMVCWebApplication.Areas.Admin.Controllers
             var categoryItem = await _context.CategoryItem.FindAsync(id);
             _context.CategoryItem.Remove(categoryItem);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Index), new { categoryId = categoryItem.CategoryId });
         }
 
         private bool CategoryItemExists(int id)
